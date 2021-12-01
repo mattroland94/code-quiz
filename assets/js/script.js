07b4faa9cdc47f502693f8b82d18b36717e94211
Matt Roland <83994674+mattroland94@users.noreply.github.com>
2021-12-01 16:30:53 -0600

    Built out the index.html page and also built the array of questions in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+let questions = [
+    {
+        ques: "Commonly used data types DO NOT include:",
+        ans: "alerts",
+        c1: "strings",
+        c2: "booleans",
+        c3: "alerts",
+        c4: "numbers"
+    },
+    {
+        ques: "The condition of an if / else statement is enclosed with ______.",
+        ans: "3. parenthesis",
+        c1: "1. quotes",
+        c2: "2. curly brackets",
+        c3: "3. parenthesis",
+        c4: "4. square brackets"
+    },
+    {
+        ques: "Arrays in JavaScript can be used to store ______.",
+        ans: "4. all of the above",
+        c1: "1. numbers and strings",
+        c2: "2. other arrays",
+        c3: "3. booleans",
+        c4: "4. all of the above"
+    },
+    {
+        ques: "String values must be enclosed within ______ when being assigned to variables.",
+        ans: "3. quotes",
+        c1: "1. commas",
+        c2: "2. curly brackets",
+        c3: "3. quotes",
+        c4: "4. parenthesis"
+    },
+    {
+        ques: "ans very useful tool used during development and debugging for printing content to the debugger is:",
+        ans: "4. console.log",
+        c1: "1. JavaScript",
+        c2: "2. terminal/bash",
+        c3: "3. for loops",
+        c4: "4. console.log"
+    }
+];
+
